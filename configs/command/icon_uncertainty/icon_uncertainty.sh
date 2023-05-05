8308bbdbfdbdf417fefd55f8f2e8d5df665cd625
youngyif <476391708@qq.com>
2023-05-05 17:25:04 +0800

    1

--- configs/command/icon_uncertainty/icon_uncertainty.sh
+++ configs/command/icon_uncertainty/icon_uncertainty.sh
@@ -1,8 +1,8 @@
 
 
-source /mnt/cephfs/home/yangyifan/miniconda/etc/profile.d/conda.sh
-# conda activate icontitanx ##icon for 3090
-conda activate icon3090
+source /mnt/cephfs/home/qiuzhen/anaconda3/etc/profile.d/conda.sh
+# conda activate /mnt/cephfs/home/qiuzhen/anaconda3/envs/icon3090v1/ 
+conda activate icon3090v1
 cd /mnt/cephfs/home/yangyifan/yangyifan/code/avatar/ICON/ 
 
 # CUDA_VISIBLE_DEVICES=6 python -m apps.train -cfg configs/train/icon_uncertainty/icon-filter_uncertainty_beta0003.yaml --gpus 0 --num_gpus 1  --uncertainty --beta_min 0.003 --beta_plus 5 --test_mode
@@ -12,4 +12,8 @@ cd /mnt/cephfs/home/yangyifan/yangyifan/code/avatar/ICON/
 
 
 # CUDA_VISIBLE_DEVICES=3,5 python -m apps.train_and_eval -cfg configs/train/icon_uncertainty/icon-filter_uncertainty.yaml --gpus 0 --num_gpus 2 --name baseline/pertubesdf0005 --perturb_sdf 0.005
-CUDA_VISIBLE_DEVICES=3 python -m apps.train_and_eval -cfg configs/train/icon_uncertainty/icon-filter_uncertainty.yaml --gpus 0 --num_gpus 1 --name baseline/pertubesdf0001 --perturb_sdf 0.001
+# CUDA_VISIBLE_DEVICES=3 python -m apps.train_and_eval -cfg configs/train/icon_uncertainty/icon-filter_uncertainty.yaml --gpus 0 --num_gpus 1 --name baseline/pertubesdf0001 --perturb_sdf 0.001
+
+# CUDA_VISIBLE_DEVICES=2 python -m apps.train_and_eval -cfg configs/train/icon_uncertainty/icon-filter_uncertainty.yaml --gpus 0 --num_gpus 1 --name baseline/pertubesdf01 --perturb_sdf 0.1
+# CUDA_VISIBLE_DEVICES=3 python -m apps.train_and_eval -cfg configs/train/icon_uncertainty/icon-filter_uncertainty.yaml --gpus 0 --num_gpus 1 --name baseline/pertubesdf02 --perturb_sdf 0.2
+CUDA_VISIBLE_DEVICES=4 python -m apps.train_and_eval -cfg configs/train/icon_uncertainty/icon-filter_uncertainty.yaml --gpus 0 --num_gpus 1 --name baseline/pertubesdf05 --perturb_sdf 0.5
